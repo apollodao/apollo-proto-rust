66cb87333618be78727052220918bd07742d4487
George McAskill <42020095+georgemc98@users.noreply.github.com>
2022-07-01 08:33:50 -0500

    feat: change path of imports

--- src/osmosis/tokenfactory/v1beta1/osmosis_tokenfactory_v1beta1.rs
+++ src/osmosis/tokenfactory/v1beta1/osmosis_tokenfactory_v1beta1.rs
@@ -7,43 +7,41 @@
 /// current admin.
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct MsgCreateDenom {
-    #[prost(string, tag="1")]
+    #[prost(string, tag = "1")]
     pub sender: ::prost::alloc::string::String,
     /// subdenom can be up to 44 "alphanumeric" characters long.
-    #[prost(string, tag="2")]
+    #[prost(string, tag = "2")]
     pub subdenom: ::prost::alloc::string::String,
 }
 /// MsgCreateDenomResponse is the return value of MsgCreateDenom
 /// It returns the full string of the newly created denom
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct MsgCreateDenomResponse {
-    #[prost(string, tag="1")]
+    #[prost(string, tag = "1")]
     pub new_token_denom: ::prost::alloc::string::String,
 }
 /// MsgMint is the sdk.Msg type for allowing an admin account to mint
 /// more of a token.  For now, we only support minting to the sender account
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct MsgMint {
-    #[prost(string, tag="1")]
+    #[prost(string, tag = "1")]
     pub sender: ::prost::alloc::string::String,
-    #[prost(message, optional, tag="2")]
-    pub amount: ::core::option::Option<super::super::super::cosmos::base::v1beta1::Coin>,
+    #[prost(message, optional, tag = "2")]
+    pub amount: ::core::option::Option<super::super::super::super::cosmos::base::v1beta1::Coin>,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
-pub struct MsgMintResponse {
-}
+pub struct MsgMintResponse {}
 /// MsgBurn is the sdk.Msg type for allowing an admin account to burn
 /// a token.  For now, we only support burning from the sender account.
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct MsgBurn {
-    #[prost(string, tag="1")]
+    #[prost(string, tag = "1")]
     pub sender: ::prost::alloc::string::String,
-    #[prost(message, optional, tag="2")]
-    pub amount: ::core::option::Option<super::super::super::cosmos::base::v1beta1::Coin>,
+    #[prost(message, optional, tag = "2")]
+    pub amount: ::core::option::Option<super::super::super::super::cosmos::base::v1beta1::Coin>,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
-pub struct MsgBurnResponse {
-}
+pub struct MsgBurnResponse {}
 // // ===================== MsgForceTransfer
 // message MsgForceTransfer {
 //   string sender = 1 [ (gogoproto.moretags) = "yaml:\"sender\"" ];
@@ -63,13 +61,12 @@ pub struct MsgBurnResponse {
 /// adminship of a denom to a new account
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct MsgChangeAdmin {
-    #[prost(string, tag="1")]
+    #[prost(string, tag = "1")]
     pub sender: ::prost::alloc::string::String,
-    #[prost(string, tag="2")]
+    #[prost(string, tag = "2")]
     pub denom: ::prost::alloc::string::String,
-    #[prost(string, tag="3")]
+    #[prost(string, tag = "3")]
     pub new_admin: ::prost::alloc::string::String,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
-pub struct MsgChangeAdminResponse {
-}
+pub struct MsgChangeAdminResponse {}
