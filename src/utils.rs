c32895a6f1c70337beed43ebc8e59034a8547610
apolloshab <lal.rishab@gmail.com>
2022-10-02 15:07:47 -0400

    feat - adds decode() util for desereliazing back to proto msg

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,10 @@
 use cosmwasm_std::Binary;
-use prost::Message;
+use prost::{DecodeError, Message};
 
 pub fn encode<T: Message>(proto_msg: T) -> Binary {
     Binary::from(proto_msg.encode_to_vec())
+}
+
+pub fn decode<T: Message + Default>(binary_msg: &Binary) -> Result<T, DecodeError> {
+    Message::decode(binary_msg.as_slice())
 }
